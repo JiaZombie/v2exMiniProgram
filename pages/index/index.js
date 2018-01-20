ce03df493f8f27d30b1120f2caa809f97ad74f7a
小僵尸 <xiaojiangshi@qiook.com>
2018-01-20 17:46:17 +0800

    2018年1月20日17点46分

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,44 +3,27 @@ var app = getApp();
 
 var config = {
     data: {
-        disable: false,
-        gameList: ['最新主题']
+        errorMsg: '',
+        themeList: []
     },
 
     onLoad: function() {
-        var that = this
-            //调用应用实例的方法获取全局数据
-        app.getUserInfo(function(userInfo) {
-            //更新数据
-            that.setData({
-                userInfo: userInfo
-            })
-        })
+        var that = this;
+        wx.request({
+          url: "https://www.v2ex.com/api/topics/hot.json",
+          success: function(res) {
+            if (res.statusCode === 200) {
+              that.data.themeList = res.data;
+            } else {
+              that.data.errorMsg = res.errMsg;
+            }
+          }
+        });
     },
     onReady: function() {
         // 页面渲染完毕
-    },
-    onShow: function() {
-        // 页面展示
-    },
-    onHide: function() {
-        // 页面隐藏
-    },
-    onUnload: function() {
-        // 页面关闭
+      var that = this;
+      console.log(that.data);
     }
 };
-
-config.data.gameList.forEach(function(v) {
-    config['start' + v] = function() {
-
-        config.data.disable = true;
-
-        // 这里需要注意每个游戏文件夹名称需和js名称保持一致
-        wx.navigateTo({
-            url: '../' + v + '/' + v
-        })
-    }
-});
-
 Page(config);
